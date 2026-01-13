1c5648091167120591a72fda39ef0f655e35f7d3
chentetro <chenush270@gmail.com>
2026-01-13 13:51:55 +0200

    Add SocialButtons component with GitHub and CV buttons

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,13 @@
+import SocialButtons from './components/SocialButtons';
+
 export default function Home() {
   return (
     <>
-    <h1>hello</h1>
+      <h1>hello</h1>
+      <SocialButtons 
+        githubUrl="https://github.com/chentetro"
+        cvUrl="/resume.pdf"
+      />
     </>
-  )};
+  );
+}
