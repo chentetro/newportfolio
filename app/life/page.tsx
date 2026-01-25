5038e0cac04294623219b829745d736d07925806
chentetro <chenush270@gmail.com>
2026-01-26 01:27:02 +0200

    navbar implementation phase 2 completed

---
+++ app/life/page.tsx
@@ -0,0 +1,13 @@
+export default function Life() {
+  return (
+    <main>
+      <section className="py-12 px-4">
+        <div className="max-w-6xl mx-auto">
+          <h1 className="text-3xl lg:text-4xl font-bold text-gray-900 dark:text-gray-100 mb-4">
+            Life
+          </h1>
+        </div>
+      </section>
+    </main>
+  );
+}
